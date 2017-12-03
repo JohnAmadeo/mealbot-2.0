a40678fa6658a24cd029f8616ce47c7b5a01663f
Bradley <bradley.yam@gmail.com>
2017-12-02 22:06:51 -0500

    Created dropzone, began http POST, have fun

--- static/js/app/Main.js
+++ static/js/app/Main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Request from 'superagent';
 import UUID from 'uuid/v4';
 import Store from 'store2';
+import Dropzone from 'react-dropzone';
+import Request from 'react-promise';
 
 class Main extends React.Component {
   constructor(props) {
@@ -11,10 +12,71 @@ class Main extends React.Component {
   render() {
     return (
       <div className='Main'>
-        Hello World!
+        <Basic/>
       </div>
     )
   }
 }
 
 module.exports = Main;
+
+class Basic extends React.Component {
+  constructor() {
+    super()
+    this.state = { files: [] }
+  }
+
+  onDrop(files) {
+    console.log(files);
+    if (!isValidUpload(files)) {
+      console.log('Error!!!! File uploaded is not a ".csv"');
+      return;
+    }
+
+    var options = {
+      method: 'POST',
+      uri: '/names',
+      formData: {
+        file: {
+          value: fs.createReadStream(files[0].name),
+          options: {
+            filename: files[0].name,
+            contentType:'/csv'
+          }
+        }
+      }
+    }
+
+    Request(options)
+
+
+    this.setState({
+      files: files
+    });
+  }
+
+  isValidUpload(files) {
+    return file[0].name.endsWith('.csv')
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="dropzone">
+          <Dropzone multiple={false} onDrop={this.onDrop.bind(this)}>
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {
+              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+
+            }
+          </ul>
+        </aside>
+      </section>
+    );
+  }
+}
